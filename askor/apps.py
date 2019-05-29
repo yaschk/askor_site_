5cddb76a7b4f2282ee912fb1bc13369c8e5fefe4
Sasha <tok29@i.ua>
2019-05-29 22:35:51 +0300

    init

---
+++ askor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AskorConfig(AppConfig):
+    name = 'askor'
---
+++ askor/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from askor.models import *
+from django.views import generic
+
+
+def index(request):
+    products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    products_images_1 = products_images.filter(product__category__id=1)
+    products_images_2 = products_images.filter(product__category__id=2)
+    products_images_3 = products_images.filter(product__category__id=3)
+    products_images_4 = products_images.filter(product__category__id=4)
+    return render(request, 'landing/index.html', locals())
+
+
+def product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'landing/product.html', locals())
+
+
+def info (request):
+
+    return render(request, 'landing/info.html', locals())
+
+
+def contacs (request):
+
+    return render(request, 'landing/contacts.html', locals())
