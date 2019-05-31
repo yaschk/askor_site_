5f6aa17839990a802304ddaec987759a00bdd5ed
Sasha <tok29@i.ua>
2019-05-31 17:56:02 +0300

    company page

--- askor_site_/settings.py
+++ askor_site_/settings.py
@@ -16,7 +16,7 @@ from configparser import RawConfigParser
 config = RawConfigParser()
 config.read('/webapps/askor_site_/settings.ini')
 
-dev = False
+dev = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
