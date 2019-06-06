da8e4e5dc9d5baeac5ca60f05cf7cf951e9e40b4
Sasha <tok29@i.ua>
2019-06-06 10:23:11 +0300

    ef

--- askor/translation.py
+++ askor/translation.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from modeltranslation.translator import register, TranslationOptions
 from .models import *
+
 
 @register(Product)
 class ProductTranslationOptions(TranslationOptions):
